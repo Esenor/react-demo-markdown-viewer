0b1fc1bbde8f090c4afc4e5423c8c09a6e3cb5dc
Esenor <daviddieu80@gmail.com>
2019-08-08 11:45:30 +0200

    updating initial markdown text

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import MarkdownEditorContainer from '../containers/TextEditor/MarkdownEditor.con
 import ToogleContainer from './ui/ToogleContainer'
 import { TextEditorProvider } from '../contexts/TextEditorContext'
 
-const initialMarkdown = '![React icon](/favicon.ico)\n\n# React demo\n\n## Markdown viewer\n\nThis repository is an React app example of:\n\n- React 16\n- React Hooks\n- React Context\n\n> The markdown parser is done with the node module `marked`\n\n> The UI is done with the node module `semantic-ui-css` and `semantic-ui-react`\n\n---'
+const initialMarkdown = '![React icon](https://facebook.github.io/create-react-app/img/logo.svg)\n\n# React demo\n\n## Markdown viewer\n\nThis repository is an React app example of:\n\n- React 16\n- React Hooks\n- React Context\n\n> The markdown parser is done with the node module `marked`\n\n> The UI is done with the node module `semantic-ui-css` and `semantic-ui-react`\n\n---'
 
 const App = () => (
   <TextEditorProvider defaultValue={initialMarkdown}>
